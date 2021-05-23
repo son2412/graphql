4c956a9209656da4a2f1b09956f64b7d6e83d253
son2412 <ngovanson196@gamil.com>
2021-05-23 21:48:54 +0700

    make seeder

--- src/seeder/20200402_222841_demo_users.js
+++ src/seeder/20200402_222841_demo_users.js
@@ -13,12 +13,32 @@ module.exports = {
         birth: '5-2-1996',
         gender: 1,
         status: 1
+      },
+      {
+        first_name: 'Ngo',
+        last_name: 'Son',
+        email: 'ngovanson196@gmail.com',
+        phone: '0379170680',
+        password: password,
+        birth: '5-2-1996',
+        gender: 1,
+        status: 1
+      },
+      {
+        first_name: 'Huong',
+        last_name: 'Nguyen',
+        email: 'shiranheika@gmail.com',
+        phone: '0332641010',
+        password: password,
+        birth: '17-11-1998',
+        gender: 1,
+        status: 1
       }
     ];
 
     return queryInterface.bulkInsert(
       'users',
-      items.map(item => Object.assign(item, { created_at: new Date(), updated_at: new Date() }))
+      items.map((item) => Object.assign(item, { created_at: new Date(), updated_at: new Date() }))
     );
   },
 
