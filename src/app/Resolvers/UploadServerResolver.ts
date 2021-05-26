14d22e8194536668711fd7c8c0397689eb37f030
son2412 <ngovanson196@gmail.com>
2021-05-26 17:50:56 +0700

    upload file server

--- src/app/Resolvers/UploadServerResolver.ts
+++ src/app/Resolvers/UploadServerResolver.ts
@@ -2,18 +2,27 @@ import { Resolver, Query, Arg, Mutation } from 'type-graphql';
 import { GraphQLUpload } from 'graphql-upload';
 import { Stream } from 'stream';
 import { createWriteStream } from 'fs';
+import * as _ from 'lodash';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Resolver()
 export class UploadServerResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => String)
   async singleUpload(
     @Arg('file', () => GraphQLUpload)
-    { createReadStream, filename }: Upload
-  ): Promise<boolean> {
+    { createReadStream, filename, mimetype, encoding }: Upload
+  ): Promise<string> {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    const file = _.kebabCase(path.basename(filename, path.extname(filename)) + Date.now()) + path.extname(filename);
     return new Promise(async (resolve, reject) =>
       createReadStream()
-        .pipe(createWriteStream(__dirname + `/../../../uploads/${filename}`))
-        .on('finish', () => resolve(true))
+        .pipe(createWriteStream(__dirname + `/../../../uploads/${file}`))
+        .on('finish', () =>
+          resolve(`${process.env.API_URL}/upload/${file}`)
+        )
         .on('error', () => reject(false))
     );
   }
